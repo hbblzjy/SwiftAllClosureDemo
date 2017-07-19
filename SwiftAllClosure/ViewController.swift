c6805f36dcd13e5976417d9418e8617ff048b03d
hbblzjy <hbblzjy@163.com>
2017-07-19 16:36:19 +0800

    Initial Commit

---
+++ SwiftAllClosure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAllClosure
+//
+//  Created by JasonHao on 2017/7/19.
+//  Copyright © 2017年 JasonHao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
